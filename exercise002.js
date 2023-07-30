aa76b62bac843c9a43661bafcd2de7badae92c55
CharlesMartinUK <charles_martin@hotmail.co.uk>
2023-07-30 15:58:43 +0100

    Did exercise 2

--- exercise002.js
+++ exercise002.js
@@ -1,12 +1,25 @@
 import fetch from "node-fetch";
 
 const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
-
+/*
 const fetchData = (apiEndPoint) => {
   fetch(apiEndPoint)
     .then((response) => response.json())
     .then((json) => console.log(json))
     .catch((error) => console.log(error));
 };
+*/
+async function test2(apiEndPoint){
 
-fetchData(jsonTypicode);
+    try{
+        let response =  await fetch(apiEndPoint)
+        let json  = await response.json()
+        console.log(json)
+        
+    }catch(err) {
+        console.log(err)
+    }
+    
+}
+
+const result = test2(jsonTypicode)
